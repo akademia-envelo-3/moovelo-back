d71a9838aa6bd8624ad525073f2917ea88a7c6a1
Norshyee <rafal.karchier@gmail.com>
2023-01-11 15:42:54 +0100

    build: Create empty project
    Create empty spring boot project with maven dependencies

---
+++ src/main/java/pl/envelo/moovelo/MooveloApplication.java
@@ -0,0 +1,13 @@
+package pl.envelo.moovelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooveloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooveloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/envelo/moovelo/MooveloApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.envelo.moovelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooveloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
