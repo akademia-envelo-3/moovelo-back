57b3b5b89c0b3534dce3ac79727a3f42c7d3407f
Rafał Karchier <rafal.karchier@ppuc.pl>
2023-01-12 16:46:01 +0100

    build: add basic configuration
    Create profiles (dev, test) and create basic configuration for each of them. Add spring security basic config. Add logs configuration

---
+++ src/main/java/pl/envelo/moovelo/TestController.java
@@ -0,0 +1,16 @@
+package pl.envelo.moovelo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String smh(){
+        log.info("testowy log");
+        return "smh";
+    }
+}
