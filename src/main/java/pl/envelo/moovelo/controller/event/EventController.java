186a5dfb991ca66a3206b5a6d3eb14c474364cfc
Ryszard Pietruszyński <ryszard.pietruszynsk@ppuc.pl>
2023-01-20 15:45:24 +0100

    feat: add empty Controller classes
    add Controller classes with Annotations

---
+++ src/main/java/pl/envelo/moovelo/controller/event/EventController.java
@@ -0,0 +1,8 @@
+package pl.envelo.moovelo.controller.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RestController
+public class EventController {
+}
