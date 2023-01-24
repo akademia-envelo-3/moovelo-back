9f163718afe091ce504bbe59f6252ed231992ca3
jkryst <j.kryst@outlook.com>
2023-01-24 16:33:19 +0100

    feat: add BasicUserMapper class

---
+++ src/main/java/pl/envelo/moovelo/controller/mapper/actor/BasicUserMapper.java
@@ -0,0 +1,22 @@
+package pl.envelo.moovelo.controller.mapper.actor;
+
+import pl.envelo.moovelo.controller.dto.actor.BasicUserDto;
+import pl.envelo.moovelo.entity.actors.BasicUser;
+
+public class BasicUserMapper {
+
+    public static BasicUser map(BasicUserDto basicUserDto) {
+
+        BasicUser basicUser = new BasicUser();
+        basicUser.setId(basicUserDto.getId());
+        basicUser.setFirstname(basicUserDto.getFirstname());
+        basicUser.setLastname(basicUserDto.getLastname());
+
+        return basicUser;
+    }
+
+    public static BasicUserDto map(BasicUser basicUser) {
+
+        return new BasicUserDto(basicUser.getFirstname(), basicUser.getLastname());
+    }
+}
---
+++ src/main/java/pl/envelo/moovelo/controller/mapper/actor/VisitorMapper.java
@@ -0,0 +1,4 @@
+package pl.envelo.moovelo.controller.mapper.actor;
+
+public class VisitorMapper {
+}
