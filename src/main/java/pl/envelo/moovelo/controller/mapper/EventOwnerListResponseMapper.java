a9f2e85c4bc51abe832cf5f305009b16be827744
Norshyee <rafal.karchier@gmail.com>
2023-01-25 16:47:36 +0100

    fix: Changed names of Dto's and Mappers for endpoint getAllEvents
    Change names and create Dto's important for endpoint

---
+++ src/main/java/pl/envelo/moovelo/controller/mapper/EventOwnerListResponseMapper.java
@@ -0,0 +1,13 @@
+package pl.envelo.moovelo.controller.mapper;
+
+import pl.envelo.moovelo.controller.dto.event.ownership.EventOwnerListResponseDto;
+import pl.envelo.moovelo.entity.events.EventOwner;
+
+public class EventOwnerListResponseMapper {
+
+    public static EventOwnerListResponseDto mapEventOwnerToEventOwnerListResponseDto(EventOwner eventOwner) {
+        return EventOwnerListResponseDto.builder()
+                .userId(eventOwner.getBasicUserId())
+                .build();
+    }
+}
