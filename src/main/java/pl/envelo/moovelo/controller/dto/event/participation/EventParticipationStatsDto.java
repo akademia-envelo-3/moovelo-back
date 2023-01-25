04b66bda6932c645eb37a59ba0900d29b859a32f
magdalena.stebel <mstebel971@gmail.com>
2023-01-25 14:26:22 +0100

    feat: add mapping methods in EventInfoMapper and modify EventParticiaprionStatsDto class
    implement mapping methods in general. Some parts of the methods missing due to other dependent tasks

--- src/main/java/pl/envelo/moovelo/controller/dto/event/participation/EventParticipationStatsDto.java
+++ src/main/java/pl/envelo/moovelo/controller/dto/event/participation/EventParticipationStatsDto.java
@@ -2,12 +2,16 @@ package pl.envelo.moovelo.controller.dto.event.participation;
 
 import lombok.Builder;
 import lombok.Getter;
+import pl.envelo.moovelo.controller.dto.actor.BasicUserDto;
+import pl.envelo.moovelo.controller.dto.actor.VisitorDto;
+
+import java.util.List;
 
 @Builder
 @Getter
 public class EventParticipationStatsDto {
-    private long eventId;
-    private int accepted;
-    private int pending;
-    private int rejected;
+    private List<BasicUserDto> accepted;
+    private List<BasicUserDto> pending;
+    private List<BasicUserDto> rejected;
+    private List<VisitorDto> visitors;
 }
--- src/main/java/pl/envelo/moovelo/controller/mapper/event/EventInfoMapper.java
+++ src/main/java/pl/envelo/moovelo/controller/mapper/event/EventInfoMapper.java
@@ -1,4 +1,40 @@
 package pl.envelo.moovelo.controller.mapper.event;
+import pl.envelo.moovelo.controller.dto.event.EventInfoDto;
+import pl.envelo.moovelo.entity.events.Event;
+import pl.envelo.moovelo.entity.events.EventInfo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class EventInfoMapper {
+    private final static DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public static EventInfoDto mapEventInfoToEventInfoDto(EventInfo eventInfo) {
+        return EventInfoDto.builder()
+                .id(eventInfo.getId())
+                .eventId(eventInfo.getId())
+                .name(eventInfo.getName())
+                .description(eventInfo.getDescription())
+//                    .category()
+                .startDate(eventInfo.getStartDate().format(DATE_FORMAT))
+                .isConfirmationRequired(eventInfo.getIsConfirmationRequired())
+//                .location()
+//                .attachments()
+                .build();
+
+    }
+    public static EventInfo mapEventInfoDtoToEventInfo(EventInfoDto eventInfoDto, long id, Event event) {
+        EventInfo eventInfo = new EventInfo();
+        eventInfo.setId(id);
+        eventInfo.setEvent(event);
+        eventInfo.setDescription(eventInfoDto.getDescription());
+        eventInfo.setStartDate(LocalDateTime.parse(eventInfoDto.getStartDate(), DATE_FORMAT));
+     //   eventInfo.setLocation(LocationMapper.mapLocationDtoToLocation(eventInfoDto.getLocation()));
+        eventInfo.setIsConfirmationRequired(eventInfoDto.isConfirmationRequired());
+        eventInfo.setCategory(CategoryMapper.mapCategoryDtoToCategory(eventInfoDto.getCategory()));
+    //    eventInfo.setFiles(eventInfoDto.getAttachments().stream().map(attachmentDto -> AttachmentMapper.mapAttachmentDtoToAttachment(attachmentDto)));
+
+
+
+    }
 }
