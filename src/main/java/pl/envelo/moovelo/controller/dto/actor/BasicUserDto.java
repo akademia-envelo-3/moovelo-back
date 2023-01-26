0efcf367187400bcf60ccc3a31ed69266114cd10
magdalena.stebel <mstebel971@gmail.com>
2023-01-26 15:20:42 +0100

    add test class to EventParticipationStats dto

--- src/main/java/pl/envelo/moovelo/controller/dto/actor/BasicUserDto.java
+++ src/main/java/pl/envelo/moovelo/controller/dto/actor/BasicUserDto.java
@@ -3,6 +3,8 @@ package pl.envelo.moovelo.controller.dto.actor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 @AllArgsConstructor
 public class BasicUserDto {
@@ -13,5 +15,18 @@ public class BasicUserDto {
     public BasicUserDto(String firstname, String lastname) {
         this.firstname = firstname;
         this.lastname = lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BasicUserDto that = (BasicUserDto) o;
+        return id == that.id && Objects.equals(firstname, that.firstname) && Objects.equals(lastname, that.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstname, lastname);
     }
 }
