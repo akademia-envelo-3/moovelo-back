4e8582811ea74e7a12e0fe9c986bdc08c5a705f0
jkryst <j.kryst@outlook.com>
2023-01-31 17:19:10 +0100

    feat: added getUsersWithAccess in EventService

--- src/main/java/pl/envelo/moovelo/service/event/EventService.java
+++ src/main/java/pl/envelo/moovelo/service/event/EventService.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.envelo.moovelo.entity.actors.BasicUser;
 import pl.envelo.moovelo.entity.events.Event;
+import pl.envelo.moovelo.entity.events.EventType;
+import pl.envelo.moovelo.entity.events.InternalEvent;
 import pl.envelo.moovelo.repository.event.EventRepository;
+import pl.envelo.moovelo.service.actors.BasicUserService;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -18,9 +22,15 @@ public class EventService {
 
     private EventRepository eventRepository;
 
+    private BasicUserService basicUserService;
+
+    private InternalEventService internalEventService;
+
     @Autowired
-    public EventService(EventRepository eventRepository) {
+    public EventService(EventRepository eventRepository, BasicUserService basicUserService, InternalEventService internalEventService) {
         this.eventRepository = eventRepository;
+        this.basicUserService = basicUserService;
+        this.internalEventService = internalEventService;
     }
 
     public List<? extends Event> getAllEvents() {
@@ -39,5 +49,26 @@ public class EventService {
         }
         log.info("EventService - getEventById() return {}", eventOptional.get());
         return eventOptional.get();
+    }
+
+    public List<BasicUser> getUsersWithAccess(Long eventId) {
+        log.info("EventService - getUsersWithAccess()");
+        Event event = getEventById(eventId);
+
+        if (event.getEventType().equals(EventType.INTERNAL_EVENT)
+                || event.getEventType().equals(EventType.CYCLIC_EVENT)) {
+
+            InternalEvent internalEvent = internalEventService.getInternalEventById(eventId);
+
+            if (internalEvent.getGroup() != null) {
+                return internalEvent.getGroup().getMembers();
+            } else {
+                return event.getUsersWithAccess();
+            }
+
+        } else {
+
+            return basicUserService.getAllBasicUsers();
+        }
     }
 }
--- src/main/java/pl/envelo/moovelo/service/event/InternalEventService.java
+++ src/main/java/pl/envelo/moovelo/service/event/InternalEventService.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.envelo.moovelo.entity.events.Event;
 import pl.envelo.moovelo.entity.events.EventType;
+import pl.envelo.moovelo.entity.events.InternalEvent;
 import pl.envelo.moovelo.repository.event.InternalEventRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -28,5 +31,15 @@ public class InternalEventService {
 
         log.info("InternalEventService - getAllInternalEvents() return {}", allInternalEvents.toString());
         return allInternalEvents;
+    }
+
+    public InternalEvent getInternalEventById(Long id) {
+        log.info("InternalEventService - getInternalEventById()");
+        Optional<? extends InternalEvent> internalEventOptional = internalEventRepository.findById(id);
+        if (internalEventOptional.isEmpty()) {
+            throw new NoSuchElementException("No internal event with id: " + id);
+        }
+        log.info("EventService - getEventById() return {}", internalEventOptional.get());
+        return internalEventOptional.get();
     }
 }
