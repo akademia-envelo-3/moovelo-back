fd9d40cddc57b208c9a86063bc557345672bf092
jkryst <j.kryst@outlook.com>
2023-02-13 12:03:45 +0100

    removed unused method from InternalEventService

--- src/main/java/pl/envelo/moovelo/service/event/InternalEventService.java
+++ src/main/java/pl/envelo/moovelo/service/event/InternalEventService.java
@@ -32,14 +32,4 @@ public class InternalEventService {
         log.info("InternalEventService - getAllInternalEvents() return {}", allInternalEvents.toString());
         return allInternalEvents;
     }
-
-    public InternalEvent getInternalEventById(Long id) {
-        log.info("InternalEventService - getInternalEventById()");
-        Optional<? extends InternalEvent> internalEventOptional = internalEventRepository.findById(id);
-        if (internalEventOptional.isEmpty()) {
-            throw new NoSuchElementException("No internal event with id: " + id);
-        }
-        log.info("EventService - getEventById() return {}", internalEventOptional.get());
-        return internalEventOptional.get();
-    }
 }
