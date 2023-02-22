3e754711c2e13f87ac46cd2a7387939ecfc35236
Norshyee <rafal.karchier@gmail.com>
2023-02-22 15:25:27 +0100

    Add cors global config

--- src/main/java/pl/envelo/moovelo/config/security/WebSecurityConfig.java
+++ src/main/java/pl/envelo/moovelo/config/security/WebSecurityConfig.java
@@ -13,6 +13,12 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -24,7 +30,7 @@ public class WebSecurityConfig {
     private AuthenticationConfiguration authenticationConfiguration;
 
     private static final String[] AUTH_WHITELIST = {
-        "/api/login/**",
+        "/login/**",
         "/api/refreshToken/**",
         "/v2/api-docs",
         "/v3/api-docs",
@@ -38,6 +44,14 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager());
         jwtAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
+        http.cors().configurationSource(request -> {
+            var cors = new CorsConfiguration();
+            cors.setAllowedOrigins(List.of("http://localhost:4200"));
+            cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            cors.setAllowedHeaders(List.of("*"));
+            return cors;
+        });
 
         return http
                 .csrf().disable()
