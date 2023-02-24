c3aae353e4a4f67f3a512cdf2b0eab08ad5b1ee1
magdalena.stebel <mstebel971@gmail.com>
2023-02-24 16:30:25 +0100

    refactor OwnershipRequest dto & GroupOwnerMapper

--- src/main/java/pl/envelo/moovelo/controller/dto/OwnershipRequestDto.java
+++ src/main/java/pl/envelo/moovelo/controller/dto/OwnershipRequestDto.java
@@ -6,11 +6,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class OwnershipRequestDto {
-    private Long newOwnerUserId;
 
-    // TODO: 24.02.2023 remove controctur if not neccessary
-//    public EventOwnershipRequestDto(Long newOwnerUserId) {
-//        this.newOwnerUserId = newOwnerUserId;
-//    }
+    private Long newOwnerUserId;
 }
 
